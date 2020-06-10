94f6b9f2446c16c516f596d9736358e438b0eaff
Vlp90 <vladimir.leloup@gmail.com>
2020-06-10 11:30:53 +0200

    setup exercice

--- js/memory.js
+++ js/memory.js
@@ -3,7 +3,13 @@ class MemoryGame {
     this.cards = cards;
     // add the rest of the class properties here
   }
+
+
   shuffleCards() {}
+
   checkIfPair(card1, card2) {}
+
+  
   isFinished() {}
+
 }
