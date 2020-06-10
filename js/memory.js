61e111f136ee6ef4a7c6ec339ba45d76a91ce223
Vlp90 <vladimir.leloup@gmail.com>
2020-06-10 12:51:50 +0200

    exercice done

--- js/memory.js
+++ js/memory.js
@@ -1,41 +1,47 @@
-// require('./index')
-
 class MemoryGame {
-  constructor(cards) {
+  constructor(cards){
     this.cards = cards;
+    // add the rest of the class properties here
     this.pickedCards = [];
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    this.cards.forEach((element, i) => {
-      let cardShuffled = Math.floor(Math.random() * this.cards.length);
-      if (i != cardShuffled) {
-        let shuffleCards = element;
-        element = this.cards[cardShuffled];
-        this.cards[cardShuffled] = shuffleCards;
-      }
-    });
+  shuffleCards(cards) {
+    let cardsIndex = this.cards.length
+    let temporalyValue = []
+    let ramdonIndex;
+
+    if (!cards) {
+      return undefined
+    }
+
+    while (0 !== cardsIndex) {
+      ramdonIndex = Math.floor(Math.random() * cardsIndex);
+      cardsIndex -= 1;
+
+      temporalyValue = this.cards[cardsIndex];
+      this.cards[cardsIndex] = this.cards[ramdonIndex];
+      this.cards[ramdonIndex] = temporalyValue
+    }
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
     this.pairsClicked += 1;
-
-    if (card1 === card2) {
+    if ( card1 === card2 ) {
       this.pairsGuessed += 1;
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   }
 
   isFinished() {
-    if (this.cards.length / 2 === this.pairsGuessed) {
+    if (this.pairsGuessed === (this.cards.length/2)) {
       return true;
     } else {
       return false;
     }
   }
 }
-
